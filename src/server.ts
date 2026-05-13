327d42e0eb4ef8c7cc09fd7a2fdb629057bccdbc
kanon8888 <mdrakibhasankanon@gmail.com>
2026-05-13 23:08:55 +0600

    two commit

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express, { type Application, type Request, type Response } from "express"
+import {Pool} from "pg"
 const app: Application = express()
 const port = 5000;
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.text());
+// app.use(express.urlencoded(extended : true));
+
+const pool = new Pool({
+    connectionString : "postgresql://neondb_owner:npg_opOPhI0bcuK5@ep-small-leaf-ap9jzmsa-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+})
 
 app.get('/', (req: Request, res: Response) => {
     // res.send('Hello World');
@@ -13,8 +20,16 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.post("/", async (req: Request, res: Response) => {
-    console.log(req.body);
-})
+    // console.log(req.body);
+    const { name, email, password } = req.body;
+    res.status(201).json({
+        message: "Created",
+        data: {
+            name,
+            email,
+        },
+    });
+});
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
