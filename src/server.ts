fe2975ae8d7263a7bcb67e89dcf94fb46302fa99
kanon8888 <mdrakibhasankanon@gmail.com>
2026-05-14 23:21:34 +0600

    ten commit

--- src/server.ts
+++ src/server.ts
@@ -89,12 +89,26 @@ app.get('/api/users/:id', async (req: Request, res: Response) => {
         const result = await pool.query(`
             SELECT * FROM users WHERE id=$1
             `, [id],);
+
+        if (result.rows.length === 0) {
+            res.status(500).json({
+                success: false,
+                message: "User not found",
+                data: {},
+            });
+        }
+
         res.status(200).json({
             success: true,
             message: "User retrived successfully!",
             data: result.rows[0],
         });
-    } catch (error) {
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error,
+        })
 
     }
 })
